b41ebb8941d8443cfca8854a233fc60bd4759d6d
DESKTOP-PKAKM3J\IAN <Iannorton01@gmail.com>
2016-10-11 13:49:57 -0400

    w00t

---
+++ PotatoThief/Assets/Scripts/GlassBreak.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GlassBreak : MonoBehaviour {
+
+	[SerializeField] Transform brokenObject; //grabs the broken version of the object to instantiate on break
+	[SerializeField] float breakForce = 10f; //the min power of the collision to break the glass
+	[SerializeField] float explosionRadius = 10f; //the radius around the center of the explosion that the explosion power is applied	
+	[SerializeField] float explosionPower = 5f; //how strong the explion is
+	[SerializeField] float upwardModifier = 1f; // the direction on the y plane the force is added
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.relativeVelocity.magnitude > breakForce)
+		{
+			Destroy (gameObject);
+			Instantiate (brokenObject, transform.position, transform.rotation);
+			brokenObject.localScale = transform.localScale;
+			Vector3 explosionPos = transform.position;
+			Collider[] colliders = Physics.OverlapSphere (explosionPos, explosionRadius); //checks the explosion radius for colliders and adds them to an array
+
+			foreach (Collider shard in colliders) //applies the explosion force to each collider in the colliders array.
+				{
+				
+				if (shard.GetComponent<Rigidbody>() != null ) //if the collider 'shard' has a rigidbody the explosion is applied.
+					shard.GetComponent<Rigidbody>().AddExplosionForce (explosionPower * collision.relativeVelocity.magnitude, explosionPos, explosionRadius, upwardModifier);
+				}
+
+		}
+
+	}
+}
---
+++ PotatoThief/Assets/Scripts/PotatoController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PotatoController : MonoBehaviour {
+	private Rigidbody rb;
+	[SerializeField] float speed = 3.0f;
+	[SerializeField] float jumpForce= 10.0f;
+
+
+	void Start () 
+	{
+		rb = GetComponent <Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		movePotato ();
+	}
+
+	void movePotato () //moves ye old potato
+	{
+		
+	float moveX = Input.GetAxis ("Horizontal");
+	float moveZ = Input.GetAxis ("Vertical");
+	float jumpY = 0.0f;
+	
+	
+
+
+		if (Input.GetKeyDown("space") && OnGround() ) 
+		{
+			jumpY = jumpForce;
+		}		
+
+		if (OnGround ()) 
+		{
+			Vector3 movement = new Vector3 (moveX, jumpY, moveZ);
+				
+			rb.AddForce (movement * speed);
+		}
+		else
+		{
+			Vector3 movement = new Vector3 (moveX, jumpY, moveZ);
+
+			rb.AddForce (movement * (speed/2));
+		}
+			
+	}
+	bool OnGround ()
+	{
+
+		return Physics.Raycast (transform.position, Vector3.down, GetComponent<MeshCollider>().bounds.extents.y + 0.1f);
+
+	}
+}
