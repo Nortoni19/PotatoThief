5c1a67fa2a6491d7ccc5c241dbe077aeb02b53d8
IAN! <Iannorton01@gmail.com>
2016-11-07 17:23:14 -0500

    Added ReWired Introduced Controller Functionality removed level

---
+++ Assets/_Scripts/WeaponController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponController : MonoBehaviour {
+    [SerializeField] GameObject player;
+    [SerializeField] Vector3 offset;
+
+	
+	
+	
+	void Update () 
+    { 
+        SetPos();
+
+	}
+   void SetPos() 
+   {
+        transform.position = player.transform.position + offset;
+   }
+}
